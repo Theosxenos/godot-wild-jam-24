fd1794c6e1ce3630a53534491d2b0f34bae94d1f
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-06-23 14:44:33 +0200

    Add tower spawning in build area.

--- src/build_area.gd
+++ src/build_area.gd
@@ -1,15 +1,21 @@
 extends Area2D
 
 @export var cell_size := Vector2(64, 64)
+@export var tower_scene: PackedScene
 
 func _on_area_entered(area: Area2D) -> void:
 	if not area is SpawnableTower:
 		return
-	
-	print("tower coords: %s" % area.global_position)		
 
 	var tower_position := area.global_position - global_position
 	var cell_coordinates := Vector2(int(tower_position.x / cell_size.x), int(tower_position.y / cell_size.y))	
+
+	await get_tree().create_timer(0.1).timeout
+	area.queue_free()
+	spawn_tower(cell_coordinates)
 	
-	area.global_position = global_position + cell_coordinates * cell_size + cell_size / 2
-	
+
+func spawn_tower(cell_coordinates: Vector2) -> void:
+	var tower := tower_scene.instantiate()
+	tower.position = cell_coordinates * cell_size + cell_size / 2
+	add_child(tower)
